761517a8b4bb46606552f1c5f65dfe9576dd894a
jhnoor <jamawadi@gmail.com>
2017-08-24 02:47:10 +0200

    rest

--- src/frontend/src/app/app.module.ts
+++ src/frontend/src/app/app.module.ts
@@ -1,4 +1,5 @@
 import { BrowserModule } from '@angular/platform-browser';
+import 'hammerjs';
 import { NgModule } from '@angular/core';
 import { HttpClientModule } from '@angular/common/http';
 import { FormsModule } from '@angular/forms';
